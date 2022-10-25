373430601b8deb7ff6a2fb7020150e56aac91cb7
Bilal Habib <bilal.habib67@gmail.com>
2022-10-25 10:15:20 +0100

    updated react router to v6
    removed routes file because routes is a reserved keyword, replaced component keyword with element, modified Route tag in routes map, moved routes to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import React from 'react';
-import {Routes} from './Routes'
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
+const routes = [];
 
 function App() {
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <Router>
+      <Routes>
+        {routes.map((route, index) => (
+          <Route key={index} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
   );
 }
 
